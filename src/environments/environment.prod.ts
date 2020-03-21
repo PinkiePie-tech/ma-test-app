15216b6650094c3bdd80ad24a2fed145a5358ff9
Sébastien Pujol <sebastien.pujol@gmail.com>
2020-03-21 15:39:27 +0100

    fix: configuration prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment: Environment = {
   envName: 'prod',
   shortEnv: 'prod',
   production: true,
-  maBackend: 'http://34.77.170.52/',
+  maBackend: 'http://34.77.170.52',
   appVersion: packageSettings?.version ?? '0.0.0',
   buildTime: new Date()
 };
